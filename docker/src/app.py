d832203b18868cec91985435a7b98bd9f0b48d2d
Marcelo.garcia.muro <mgarciam200593@gmail.com>
2023-02-28 14:36:51 -0500

    Flask API v1

---
+++ docker/src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/DevOps', methods=['POST'])
+def post_message():
+    data = request.json
+    return jsonify({"message": "Hello " + data['to'] + " your message will be send"})
+
+if __name__ == '__main__':
+    app.run()
